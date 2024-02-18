c8213216d4752edf72743f503a373ab9459316d0
Sayed Rafiul Islam <sayed80@student.sust.edu>
2024-02-19 01:51:46 +0600

    a try

--- app/redux/slice.ts
+++ app/redux/slice.ts
@@ -3,11 +3,35 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+interface User {
+    _id : string,
+    email: string,
+    userName: string,
+    role: string,
+    password : string
+}
+
+interface Task {
+    _id : string,
+    taskName : string,
+    taskDescription : string,
+    assignedBy : string,
+    assignedTo : string,
+    status : boolean
+  }
+
+interface RootState {
+    user: User[];
+    assignedTasks: Task[];
+    myTasks: Task[];
+    users: User[]
+  }
+
 
 const { createSlice, current } = require("@reduxjs/toolkit");
 
-const initialState = {
-    user : typeof window !== "undefined" && (localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : []),
+const initialState : RootState = {
+    user : typeof window !== "undefined" && (localStorage.getItem("user") ? JSON.parse(localStorage.getItem<>("user")) : []),
     assignedTasks : typeof window !== "undefined" && (localStorage.getItem("assignedTasks") ? JSON.parse(localStorage.getItem("assignedTasks")) : []),
     myTasks : typeof window !== "undefined" && (localStorage.getItem("myTasks") ? JSON.parse(localStorage.getItem("myTasks")) : []),
     users : typeof window !== "undefined" && (localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : []),
@@ -37,22 +61,22 @@ export const loginUser = createAsyncThunk("loginUser", async (userData) => {
 
 // task
 
-export const createTask = createAsyncThunk("createTask", async (newTask) => {
+export const createTask = createAsyncThunk("createTask", async (newTask : Task) => {
     const {data,status} = await axios.post(`${process.env.NEXT_PUBLIC_API}/createTask`,newTask)
     return data
 })
-export const getAssignedTasks = createAsyncThunk("getAssignedTasks", async (email) => {
+export const getAssignedTasks = createAsyncThunk("getAssignedTasks", async (email : string) => {
     const {data,status} = await axios(`${process.env.NEXT_PUBLIC_API}/getAssignedTasks?email=${email}`)
     localStorage.removeItem("assignedTasks")
     localStorage.setItem("assignedTasks", JSON.stringify(data))
     return data
 })
 
-export const updateMyTask = createAsyncThunk("updateMyTask", async (updatedTask) => {
+export const updateMyTask = createAsyncThunk("updateMyTask", async (updatedTask : Task) => {
     const {data,status} = await axios.patch(`${process.env.NEXT_PUBLIC_API}/updateMyTask`,updatedTask)
     return data
 })
-export const getMyTasks = createAsyncThunk("getMyTasks", async (email) => {
+export const getMyTasks = createAsyncThunk("getMyTasks", async (email : string) => {
     const {data,status} = await axios(`${process.env.NEXT_PUBLIC_API}/getMyTasks?email=${email}`)
     localStorage.removeItem("myTasks")
     localStorage.setItem("myTasks", JSON.stringify(data))
@@ -68,17 +92,17 @@ export const getAllUsers = createAsyncThunk("getAllUsers", async () => {
     return data
 })
 
-export const createUser = createAsyncThunk("createUser", async (newUser) => {
+export const createUser = createAsyncThunk("createUser", async (newUser : User) => {
     const {data,status} = await axios.post(`${process.env.NEXT_PUBLIC_API}/createUser`,newUser)
     return data
 })
 
-export const updateRole = createAsyncThunk("updateRole", async (updatedUser) => {
+export const updateRole = createAsyncThunk("updateRole", async (updatedUser : User) => {
     const {data,status} = await axios.patch(`${process.env.NEXT_PUBLIC_API}/updateRole`,updatedUser)
     return data
 })
 
-export const removeUser = createAsyncThunk("removeUser", async (_id) => {
+export const removeUser = createAsyncThunk("removeUser", async (_id : string) => {
     const {data,status} = await axios.delete(`${process.env.NEXT_PUBLIC_API}/removeUser?_id=${_id}`)
     return data
 })
@@ -89,20 +113,20 @@ const Slice = createSlice({
     initialState,
     reducers : {
         // auth 
-        logoutUser : (state , action ) => {
+        logoutUser : (state : any, action : any) => {
             window.location.assign("/authentication")
             typeof window !== "undefined" && localStorage.removeItem("user")
         },
         // ---------------------------------------------------------------------------------------------------
 
         // tasks
-        addTaskLocal : (state , action ) => {
+        addTaskLocal : (state : any, action : any) => {
             state.assignedTasks.push(action.payload)
             localStorage.removeItem("assignedTasks")
             localStorage.setItem("assignedTasks", JSON.stringify(current(state.assignedTasks)))
         },
-        updateTaskLocal : (state , action ) => {
-            const data = state.assignedTasks.filter((task ) => task._id !== action.payload.taskId)
+        updateTaskLocal : (state : any, action : any) => {
+            const data = state.assignedTasks.filter((task : Task) => task._id !== action.payload.taskId)
             const updatedData = {
                 _id : action.payload.taskId,
                 taskName : action.payload.taskName,
@@ -114,44 +138,44 @@ const Slice = createSlice({
             localStorage.removeItem("assignedTasks")
             localStorage.setItem("assignedTasks", JSON.stringify(data))
         },
-        removeTaskLocal : (state , action ) => {
-            const data = state.assignedTasks.filter((task ) => task._id !== action.payload[0]._id)
+        removeTaskLocal : (state : any, action : any) => {
+            const data = state.assignedTasks.filter((task : Task) => task._id !== action.payload[0]._id)
             localStorage.removeItem("assignedTasks")
             localStorage.setItem("assignedTasks", JSON.stringify(data))
             state.assignedTasks = data
         },
-        updateMyTaskLocal : (state , action ) => {
-            const index = state.myTasks.findIndex((task) => task._id === action.payload.id)
+        updateMyTaskLocal : (state : any, action : any) => {
+            const index = state.myTasks.findIndex((task : Task) => task._id === action.payload.id)
             state.myTasks[index].status = action.payload.status
         },
         // ---------------------------------------------------------------------------------------------------
     },
-    extraReducers : (builder ) => {
+    extraReducers : (builder : any) => {
         // auth -----------------------------------------------------------------
-        builder.addCase(signupUser.fulfilled,(state ,action ) => {
+        builder.addCase(signupUser.fulfilled,(state : any,action : any) => {
             state.isLoading = false,
             state.user = action.payload
         }),
-        builder.addCase(loginUser.fulfilled,(state, action) => {
+        builder.addCase(loginUser.fulfilled,(state : any, action : any) => {
             state.isLoading = false,
             state.user = action.payload     
         }),
         // ----------------------------------------------------------------------------
 
         // tasks --------------------------------------------------------------------------------------
-        builder.addCase(getAssignedTasks.fulfilled,(state, action) => {
+        builder.addCase(getAssignedTasks.fulfilled,(state : any, action : any) => {
             state.isLoading = false,
             state.assignedTasks = action.payload           
         }),
-        builder.addCase(getMyTasks.fulfilled,(state, action) => {
+        builder.addCase(getMyTasks.fulfilled,(state : any, action : any) => {
             state.isLoading = false,
             state.myTasks = action.payload           
         }),
-        builder.addCase(getAllUsers.fulfilled,(state, action) => {
+        builder.addCase(getAllUsers.fulfilled,(state : any, action : any) => {
             state.isLoading = false,
             state.users = action.payload           
         }),
-        builder.addCase(createTask.fulfilled,(state, action) => {
+        builder.addCase(createTask.fulfilled,(state : any, action : any) => {
             state.isLoading = false,
             state.assignedTasks.push(action.payload)      
             localStorage.removeItem("assignedTasks")
@@ -159,29 +183,29 @@ const Slice = createSlice({
         }),
 
         //  user ----------------------------------------------------------------------------------------
-        builder.addCase(createUser.fulfilled,(state, action) => {
+        builder.addCase(createUser.fulfilled,(state : any, action : any) => {
             state.isLoading = false,
             state.users.push(action.payload)      
             localStorage.removeItem("users")
             localStorage.setItem("users", JSON.stringify(current(state.users)))    
         }),
-        builder.addCase(removeUser.fulfilled,(state, action) => {
+        builder.addCase(removeUser.fulfilled,(state : any, action : any) => {
             state.isLoading = false
-            const data = state.users?.filter((user) => user._id !== action.payload._id)
+            const data = state.users?.filter((user : User) => user._id !== action.payload._id)
             state.users = data
             localStorage.removeItem("users")
             localStorage.setItem("users", JSON.stringify(data))    
         })
-        builder.addCase(updateRole.fulfilled,(state, action) => {
+        builder.addCase(updateRole.fulfilled,(state : any, action : any) => {
             state.isLoading = false
-            const index = state.users.findIndex(user => user._id === action.payload._id)
+            const index = state.users.findIndex((user : User) => user._id === action.payload._id)
             state.users[index] = action.payload
             localStorage.removeItem("users")
             localStorage.setItem("users", JSON.stringify(current(state.users)))    
         })
-        builder.addCase(updateMyTask.fulfilled,(state, action) => {
+        builder.addCase(updateMyTask.fulfilled,(state : any, action : any) => {
             state.isLoading = false
-            const index = state.myTasks.findIndex(task => task._id === action.payload._id)
+            const index = state.myTasks.findIndex((task : Task) => task._id === action.payload._id)
             state.myTasks[index] = action.payload
             localStorage.removeItem("myTasks")
             localStorage.setItem("myTasks", JSON.stringify(current(state.myTasks)))    
